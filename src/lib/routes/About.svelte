7a2dcf58963a127c2bec3fa13a1c7fe50aa33542
Thomas Junk <thomas.junk@intevation.de>
2024-02-29 16:49:10 +0100

    inital commit

---
+++ src/lib/routes/About.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { push, pop, replace } from "svelte-spa-router";
+  import { location } from "svelte-spa-router";
+  const routeToHome = () => {
+    push("/");
+  };
+</script>
+
+<h1>About page</h1>
+
+<button on:click={routeToHome}>Home</button>
+<p>{$location}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Home from "$lib/routes/Home.svelte";
+  import About from "$lib/routes/About.svelte";
+  import Router from "svelte-spa-router";
+  import { link } from "svelte-spa-router";
+  import active from "svelte-spa-router/active";
+
+  const routes = {
+    // Exact path
+    "/": Home,
+    "/about": About
+  };
+</script>
+
+<div class="container">
+  <div class="aside">
+    <h1>Welcome to $APP</h1>
+    <ul class="menu">
+      <li><a href="/" use:link use:active={"/"}>Home</a></li>
+      <li><a href="/about" use:link use:active={"/about"}>About</a></li>
+    </ul>
+  </div>
+  <div>
+    <Router {routes} />
+  </div>
+</div>
+
+<style>
+  :root {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  :global(a.active) {
+    font-weight: bold;
+    font-size: large;
+  }
+  .container {
+    display: flex;
+    flex-direction: row;
+  }
+  .aside {
+    background: blue;
+    color: white;
+    height: 100dvh;
+  }
+  .menu a {
+    color: white;
+  }
+</style>
